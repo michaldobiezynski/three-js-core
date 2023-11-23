e3f6a799a05fe70af1abbed3b95a93b672f24c1b
michaldobiezynski <michaldobiezynski@hotmail.com>
2023-11-23 20:26:53 +0000

    zoom controls

--- 12-controls-starter/src/App.jsx
+++ 12-controls-starter/src/App.jsx
@@ -10,6 +10,8 @@ function App() {
           maxPolarAngle={Math.PI / 2}
           minAzimuthAngle={-Math.PI / 2}
           maxAzimuthAngle={Math.PI / 2}
+          minDistance={3}
+          maxDistance={10}
           // enableZoom={false}
           // rotateSpeed={0.55}
         />
