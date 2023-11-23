1ca25064e2376b4d0637b4a65c31a201ca0c5eb4
michaldobiezynski <michaldobiezynski@hotmail.com>
2023-11-23 21:04:57 +0000

    add keyboard controls and key - name map

--- 13-events-starter/src/App.jsx
+++ 13-events-starter/src/App.jsx
@@ -1,12 +1,64 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { KeyboardControls } from "@react-three/drei";
+import { useMemo } from "react";
+
+export const Controls = {
+  forward: "forward",
+  backward: "backward",
+  left: "left",
+  right: "right",
+  up: "up",
+  down: "down",
+  zoomIn: "zoomIn",
+  zoomOut: "zoomOut",
+};
 
 function App() {
+  const map = useMemo(() => {
+    return [
+      {
+        name: Controls.forward,
+        keys: ["KeyW", "ArrowUp"],
+      },
+      {
+        name: Controls.backward,
+        keys: ["KeyS", "ArrowDown"],
+      },
+      {
+        name: Controls.left,
+        keys: ["KeyA", "ArrowLeft"],
+      },
+      {
+        name: Controls.right,
+        keys: ["KeyD", "ArrowRight"],
+      },
+      {
+        name: Controls.up,
+        keys: ["Space"],
+      },
+      {
+        name: Controls.down,
+        keys: ["ShiftLeft"],
+      },
+      {
+        name: Controls.zoomIn,
+        keys: ["KeyZ"],
+      },
+      {
+        name: Controls.zoomOut,
+        keys: ["KeyX"],
+      },
+    ];
+  });
+
   return (
     <>
-      <Canvas camera={{ position: [0, 1, 8], fov: 42 }}>
-        <Experience />
-      </Canvas>
+      <KeyboardControls map={map}>
+        <Canvas camera={{ position: [0, 1, 8], fov: 42 }}>
+          <Experience />
+        </Canvas>
+      </KeyboardControls>
     </>
   );
 }
