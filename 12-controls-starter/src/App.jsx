20ced0ad5099d0435a880b45a0e3b5c985456464
michaldobiezynski <michaldobiezynski@hotmail.com>
2023-11-23 20:26:23 +0000

    orbit controls - angles

--- 12-controls-starter/src/App.jsx
+++ 12-controls-starter/src/App.jsx
@@ -5,7 +5,14 @@ function App() {
   return (
     <>
       <Canvas camera={{ position: [-1.5, 3, 10], fov: 42 }}>
-        <OrbitControls />
+        <OrbitControls
+          enablePan={false}
+          maxPolarAngle={Math.PI / 2}
+          minAzimuthAngle={-Math.PI / 2}
+          maxAzimuthAngle={Math.PI / 2}
+          // enableZoom={false}
+          // rotateSpeed={0.55}
+        />
         <Lighthouse position-y={-1} scale={[0.2, 0.2, 0.2]} />
         <Environment preset="sunset" />
       </Canvas>
