55fdcdc087c57fba95178e1ef60931410f7a34c4
michaldobiezynski <michaldobiezynski@hotmail.com>
2023-11-23 20:47:15 +0000

    pointer, hover and click events

--- 13-events-starter/src/components/MoveableSphere.jsx
+++ 13-events-starter/src/components/MoveableSphere.jsx
@@ -1,8 +1,22 @@
+import React, { useState } from "react";
+
 export const MoveableSphere = (props) => {
+  const [hovered, setHovered] = useState(false);
+  const [selected, setSelected] = useState(false);
+  let color = hovered ? "pink" : "white";
+  if (selected) {
+    color = "hotpink";
+    console.log("selected");
+  }
+
   return (
-    <mesh {...props}>
+    <mesh
+      {...props}
+      onPointerEnter={() => setHovered(true)}
+      onPointerLeave={() => setHovered(false)}
+      onClick={() => setSelected(!selected)}>
       <sphereGeometry args={[0.5, 64, 64]} />
-      <meshStandardMaterial color={"white"} />
+      <meshStandardMaterial color={color} />
     </mesh>
   );
 };
