45185c5b14d72f8558a3af2b4e9eb2812fea2f88
michaldobiezynski <michaldobiezynski@hotmail.com>
2023-11-26 18:10:20 +0000

    render the generated model component instead of importing it

--- 14-models-starter/src/components/Experience.jsx
+++ 14-models-starter/src/components/Experience.jsx
@@ -1,14 +1,21 @@
-import { useGLTF } from "@react-three/drei";
+import { useFBX, useGLTF, Gltf } from "@react-three/drei";
+
+import { Fish } from "./Fish";
+
 // import { useLoader } from "@react-three/fiber";
 // import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 export const Experience = () => {
   const { scene } = useGLTF("/models/Fish.gltf");
+  const dino = useFBX("/models/Dino.fbx");
   // const model = useLoader(GLTFLoader, "/models/Fish.gltf");
 
   return (
     <>
-      <primitive object={scene} />
+      <ambientLight intensity={0.3} />
+      {/* <primitive object={scene} /> */}
+      <Fish />
+      {/* <primitive object={dino} scale={0.01} position-x={-3} /> */}
     </>
   );
 };
