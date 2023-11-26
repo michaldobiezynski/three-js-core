a0d0e6e2119fc1ec1dd59965376bfe50b1ddc2e8
michaldobiezynski <michaldobiezynski@hotmail.com>
2023-11-26 18:59:39 +0000

    import and play around with a texture

--- 15-textures-starter/src/components/Experience.jsx
+++ 15-textures-starter/src/components/Experience.jsx
@@ -1,9 +1,28 @@
+import { useTexture, useVideoTexture } from "@react-three/drei";
+
+import * as THREE from "three";
+
 export const Experience = () => {
+  // const props = useTexture({
+  //   map: "/textures/PavingStones130_1K_Color.jpg",
+  //   normalMap: "/textures/PavingStones130_1K_NormalGL.jpg",
+  //   roughnessMap: "/textures/PavingStones130_1K_Roughness.jpg",
+  //   // displacementMap: "/textures/PavingStones130_1K_Displacement.jpg",
+  //   aoMap: "/textures/PavingStones130_1K_AmbientOcclusion.jpg",
+  // });
+  // texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+  // texture.rotation = Math.PI / 4;
+  // texture.repeat.set(0.2, 0.2);
+
+  // const texture = useTexture("/textures/matcapTexture.png");
+  const texture = useVideoTexture("/textures/spongebob-squarepants.mp4");
+
   return (
     <>
       <mesh>
         <boxGeometry />
-        <meshStandardMaterial color={"white"} />
+        {/* <meshMatcapMaterial matcap={texture} /> */}
+        <meshBasicMaterial map={texture} />
       </mesh>
     </>
   );
