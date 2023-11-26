1999fabb22a276ab431c29a506e1d22d24f029bc
michaldobiezynski <michaldobiezynski@hotmail.com>
2023-11-26 17:47:45 +0000

    load fish model

--- 14-models-starter/src/components/Experience.jsx
+++ 14-models-starter/src/components/Experience.jsx
@@ -1,10 +1,14 @@
+import { useGLTF } from "@react-three/drei";
+// import { useLoader } from "@react-three/fiber";
+// import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
 export const Experience = () => {
+  const { scene } = useGLTF("/models/Fish.gltf");
+  // const model = useLoader(GLTFLoader, "/models/Fish.gltf");
+
   return (
     <>
-      <mesh>
-        <boxGeometry />
-        <meshStandardMaterial color="white" />
-      </mesh>
+      <primitive object={scene} />
     </>
   );
 };
