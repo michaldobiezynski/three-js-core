f3cead7b464028803353cb1d496aa94731e4abdb
michaldobiezynski <michaldobiezynski@hotmail.com>
2023-11-27 20:53:17 +0000

    replace cube loader with loading screen

--- 18-loading-screen-starter/src/App.jsx
+++ 18-loading-screen-starter/src/App.jsx
@@ -1,25 +1,28 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { useProgress } from "@react-three/drei";
 
 import { Suspense } from "react";
 
-const CubeLoader = () => {
+const LoadingScreen = () => {
+  const { isReady, progress } = useProgress();
+
   return (
-    <mesh>
-      <boxBufferGeometry args={[1, 1, 1]} />
-      <meshNormalMaterial color={"hotpink"} />
-    </mesh>
+    <div className="loading-screen">
+      <div className="loading-screen__container">
+        <h1 className="loading-screen_title">3D Web Agency</h1>
+        <p>Loading... {parseInt(progress)}</p>
+      </div>
+    </div>
   );
 };
-
-const LoadingScreen = () => {};
 
 function App() {
   return (
     <>
       <LoadingScreen />
       <Canvas camera={{ position: [-4, 4, 12], fov: 40 }}>
-        <Suspense fallback={<CubeLoader />}>
+        <Suspense>
           <group position-y={-1}>
             <Experience />
           </group>
