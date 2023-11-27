d4fc18877606822baaf2f51e9568e968b8f525c9
michaldobiezynski <michaldobiezynski@hotmail.com>
2023-11-27 20:31:00 +0000

    use suspense to show cube while models load

--- 18-loading-screen-starter/src/App.jsx
+++ 18-loading-screen-starter/src/App.jsx
@@ -1,13 +1,26 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
 
+import { Suspense } from "react";
+
+const CubeLoader = () => {
+  return (
+    <mesh>
+      <boxBufferGeometry args={[1, 1, 1]} />
+      <meshNormalMaterial color={"hotpink"} />
+    </mesh>
+  );
+};
+
 function App() {
   return (
     <>
       <Canvas camera={{ position: [-4, 4, 12], fov: 30 }}>
-        <group position-y={-1}>
-          <Experience />
-        </group>
+        <Suspense fallback={<CubeLoader />}>
+          <group position-y={-1}>
+            <Experience />
+          </group>
+        </Suspense>
       </Canvas>
     </>
   );
