f51796ff866840a15d80008cc77d78bdd9bdef34
michaldobiezynski <michaldobiezynski@hotmail.com>
2023-11-27 20:59:53 +0000

    style the loading screen

--- 18-loading-screen-starter/src/App.jsx
+++ 18-loading-screen-starter/src/App.jsx
@@ -5,13 +5,17 @@ import { useProgress } from "@react-three/drei";
 import { Suspense } from "react";
 
 const LoadingScreen = () => {
-  const { isReady, progress } = useProgress();
+  const { progress, active } = useProgress();
 
   return (
-    <div className="loading-screen">
+    <div className={`loading-screen ${active ? "" : "loading-screen--hidden"}`}>
       <div className="loading-screen__container">
-        <h1 className="loading-screen_title">3D Web Agency</h1>
-        <p>Loading... {parseInt(progress)}</p>
+        <h1 className="loading-screen__title">3D Web Agency</h1>
+        <div className="progress__container">
+          <div
+            className="progress__bar"
+            style={{ width: `${progress}%` }}></div>
+        </div>
       </div>
     </div>
   );
