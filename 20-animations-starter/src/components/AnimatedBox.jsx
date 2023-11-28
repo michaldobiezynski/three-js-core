325754c8111d6c50dbfb222e319eaa717c646d81
michaldobiezynski <michaldobiezynski@hotmail.com>
2023-11-28 20:28:50 +0000

    animate the box using the clock time from the useFrame hook

--- 20-animations-starter/src/components/AnimatedBox.jsx
+++ 20-animations-starter/src/components/AnimatedBox.jsx
@@ -1,8 +1,20 @@
 import { RoundedBox } from "@react-three/drei";
 import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
 
 export const AnimatedBox = ({ boxPositions, ...props }) => {
   const box = useRef();
+
+  useFrame(({ clock }) => {
+    const time = clock.getElapsedTime();
+    const x = Math.sin(time) * 2;
+    const y = Math.cos(time) * 2;
+    const z = Math.sin(time) * 2;
+
+    box.current.position.x = x;
+    box.current.position.y = y;
+    box.current.position.z = z;
+  });
 
   return (
     <group {...props}>
@@ -11,8 +23,7 @@ export const AnimatedBox = ({ boxPositions, ...props }) => {
         position-x={boxPositions[0].x}
         position-y={boxPositions[0].y}
         position-z={boxPositions[0].z}
-        ref={box}
-      >
+        ref={box}>
         <meshStandardMaterial color="white" />
       </RoundedBox>
     </group>
