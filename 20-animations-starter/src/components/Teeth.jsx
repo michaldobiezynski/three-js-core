120fade4edbadb08505428c2f9aa3065a34d480a
michaldobiezynski <michaldobiezynski@hotmail.com>
2023-11-28 21:11:09 +0000

    start animating teeth with gasp

--- 20-animations-starter/src/components/Teeth.jsx
+++ 20-animations-starter/src/components/Teeth.jsx
@@ -1,18 +1,57 @@
+import { motion } from "framer-motion-3d";
+import { useEffect, useState } from "react";
+
 export const Teeth = () => {
+  const [variant, setVarianrt] = useState("closed");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setVarianrt(variant === "closed" ? "open" : "closed");
+    }, 1500);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <group>
-      <mesh position-x={-1} position-y={-1}>
+    <motion.group animate={variant}>
+      <motion.mesh
+        position-x={-1}
+        position-y={-1}
+        variants={{
+          closed: {
+            y: -1,
+            rotateY: 0,
+          },
+          open: {
+            y: 0,
+            rotateY: Math.PI / 2,
+            scale: 1.1,
+          },
+        }}
+        transition={{
+          repeat: Infinity,
+          repeatDelay: 1,
+        }}>
+        <coneGeometry args={[0.5, 1, 4]} />
+        <motion.meshStandardMaterial
+          color="#ffffff"
+          variants={{
+            closed: {
+              color: "#7564a4",
+            },
+            open: {
+              color: "#ffffff",
+            },
+          }}
+        />
+      </motion.mesh>
+      <motion.mesh position-x={0} position-y={1} rotation-x={Math.PI}>
         <coneGeometry args={[0.5, 1, 4]} />
         <meshStandardMaterial color="#ffffff" />
-      </mesh>
-      <mesh position-x={0} position-y={1} rotation-x={Math.PI}>
+      </motion.mesh>
+      <motion.mesh position-x={1} position-y={-1}>
         <coneGeometry args={[0.5, 1, 4]} />
         <meshStandardMaterial color="#ffffff" />
-      </mesh>
-      <mesh position-x={1} position-y={-1}>
-        <coneGeometry args={[0.5, 1, 4]} />
-        <meshStandardMaterial color="#ffffff" />
-      </mesh>
-    </group>
+      </motion.mesh>
+    </motion.group>
   );
 };
